3563432be7c30dd8c7180292a0fc9142b459a287
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-06 23:46:50 +0530

    updated /update route for zod

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -11,8 +11,8 @@ const { ZodError } = require("zod");
 // in zod everything is -- required by default-- if something is optional, you have to -- mention, that is optional
 
 const TaskSchema = z.object({
-  title: z.string().min(5),
-  description: z.string(),
+  title: z.string().min(5).default("null"),
+  description: z.string().default("null"),
   completed: z.boolean().default(false),
 });
 ////////////////////
@@ -44,12 +44,14 @@ router.post("/create", async (req: Request, res: Response) => {
     //
     // type OneTask = z.infer<typeof TaskSchema>;
     // Validate the request body using Zod schema
+
     const validatedData = TaskSchema.parse({
       title: title,
       description: description,
     });
 
     //
+
     const newTask = await Task.create(validatedData);
     //
     console.log(newTask, "newTask");
@@ -118,11 +120,22 @@ router.put("/updateOne/:id", async (req: Request, res: Response) => {
 
     // console.log(req.body, "req.body");
 
-    if (title) {
+    //
+    const validatedData = TaskSchema.parse({
+      title: title,
+      description: description,
+    });
+
+    //
+    // console.log(validatedData, "validatedData");
+
+    if (title && title != null) {
       //
-      if (typeof title !== "string") {
-        return res.status(400).json({ error: "Invalid title " });
-      }
+      // if (typeof title !== "string") {
+      //   return res.status(400).json({ error: "Invalid title " });
+      // }
+      //
+      console.log(validatedData, "validatedData from title block");
       //
       const updateTitle = await Task.findByIdAndUpdate(
         req.params.id,
@@ -136,11 +149,13 @@ router.put("/updateOne/:id", async (req: Request, res: Response) => {
     }
 
     //
-    if (description) {
+    if (description && description != null) {
       //
-      if (typeof description !== "string") {
-        return res.status(400).json({ error: "Invalid description " });
-      }
+      // if (typeof description !== "string") {
+      //   return res.status(400).json({ error: "Invalid description " });
+      // }
+      //
+      console.log(validatedData, "validatedData from description block");
       //
       const updateDescription = await Task.findByIdAndUpdate(
         req.params.id,
@@ -161,9 +176,13 @@ router.put("/updateOne/:id", async (req: Request, res: Response) => {
     const updatedTask = await Task.findOne({ _id: req.params.id });
     //
     res.json({ messageArray: messageArray, updatedTask: updatedTask });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Unable to get tasks from database" });
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.log(err.issues[0], "error message from catch block");
+      return res.json({ message: err.issues[0].message });
+    }
+    console.log(err);
+    res.json({ error: "Unable to create tasks" });
   }
 });
 //
